a151e3446c4b9fd494c7089f1efad702d394c0f1
Daming Yang <daming.yang@centurygame.com>
2022-08-17 11:14:55 +0800

    refactor: add support for gin

---
+++ option.go
@@ -0,0 +1,35 @@
+package xmonitor
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+//go:generate optiongen --new_func=NewCollectorConf --xconf=true --empty_composite_nil=true --usage_tag_name=usage
+func CollectorConfOptionDeclareWithDefault() interface{} {
+	return map[string]interface{}{
+		// annotation@ConstLabels(comment="监控默认添加的静态labels")
+		"ConstLabels": map[string]string{},
+		// annotation@MonitorRegister(xconf="-"，comment="监控注册函数")
+		"MonitorRegister": MonitorRegisterFunc(nil),
+		// annotation@Buckets(comment="histogram buckets 监控耗时桶(单位秒)，参考 https://cloud.tencent.com/developer/article/1495303")
+		"Buckets": []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
+		// annotation@Skip(xconf="-"，comment="是否跳过监控")
+		"Skip": SkipFunc(func(_ *http.Request) bool {
+			return false
+		}),
+		"IgnoreLatency": IgnoreLatencyFunc(func(code int) bool {
+			return code != http.StatusOK
+		}),
+	}
+}
+
+// MonitorRegisterFunc monitor collector register
+type MonitorRegisterFunc func(prometheus.Collector)
+
+// SkipFunc if skip monitor return true else return false
+type SkipFunc func(r *http.Request) bool
+
+// IgnoreLatencyFunc ignore latency func
+type IgnoreLatencyFunc func(code int) bool
